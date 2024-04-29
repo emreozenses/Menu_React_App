2eddb61d8fce6ce7a041514c458285d5d31c21f5
Emre OZENSES <emreozenses@gmail.com>
2024-04-29 19:09:23 +0300

    buttons and filter function added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Title from "./Title";
 import menu from "./data";
 import Menu from "./Menu";
+import Categories from "./Categories";
 /*
 const tempCategories = menu.map((item)=>item.category);
 const tempSet = new Set(tempCategories);
@@ -9,45 +10,26 @@ const tempItem = ['all',...tempSet];*/
 
 
 const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
-console.log(allCategories);
 
 const App = () => {
 
 const [data,setData] = useState(menu);
 const [categories,setCategories] = useState(allCategories);
+const filterItems =(category)=>{
+  if(category==='all'){
+    setData(menu);
+    return;
+  }
+  const newItems = menu.filter((item)=>category === item.category);
+  setData(newItems);
+}
 
-const allButton=()=>{
-  setData(data);
-};
-const breakfastButton = () => {
-  data.filter((item)=>{
-    const breakfast = item.category.includes('breakfast');
-    console.log(breakfast);
-    return setData(breakfast); 
-  })
-  
-};
-const lunchButton = () => {};
-const shakesButton = () => {};
 
   return (
     <main>
       <section className="menu">
         <Title text="our menu" />
-        <div className="btn-container">
-          <button type="button" className="btn" onClick={allButton}>
-            All
-          </button>
-          <button type="button" className="btn" onClick={breakfastButton}>
-            Breakfast
-          </button>
-          <button type="button" className="btn" onClick={lunchButton}>
-            Lunch
-          </button>
-          <button type="button" className="btn" onClick={shakesButton}>
-            Shakes
-          </button>
-        </div>
+        <Categories categories={categories} filterItems={filterItems}/>
         <Menu data={data} />
       </section>
     </main>
